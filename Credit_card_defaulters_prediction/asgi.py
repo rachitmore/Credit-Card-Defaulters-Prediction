af1bfd0ed3b1658e9531b59d02ccde527b9ac951
Rachit More <rachitmore3@gmail.com>
2024-08-07 16:00:34 +0530

    File has been added
    Django project has been created
    django home app has been created
    machine learning training and prediction pipeline have been created

---
+++ Credit_card_defaulters_prediction/asgi.py
@@ -0,0 +1,18 @@
+"""
+ASGI config for Credit_card_defaulters_prediction project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "Credit_card_defaulters_prediction.settings"
+)
+
+application = get_asgi_application()
