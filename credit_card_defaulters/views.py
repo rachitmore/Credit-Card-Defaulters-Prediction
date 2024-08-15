965b416cd1935f5d74831415445ed78104f37eef
Rachit More <rachitmore3@gmail.com>
2024-08-15 19:52:19 +0530

    logging and exception handling have been implemented in django home application

--- credit_card_defaulters/views.py
+++ credit_card_defaulters/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from app_tracking.logger import App_Logger
+from app_tracking.exception import AppException
 
 # Create your views here.
 from django.shortcuts import render
